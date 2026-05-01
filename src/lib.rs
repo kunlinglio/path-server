2ea4e653798875995fe1e65e26bfd993aff3f1e6
kunlinglio <142668432+kunlinglio@users.noreply.github.com>
2026-05-01 15:35:46 +0800

    Prepare for publishing to crates.io

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! **Path Server** is an LSP server for path completion.
+//!
+//! **⚠️ WARNING: Internal API**
+//!
+//! This crate is primarily designed to be distributed as a standalone binary. And has no intention to maintain as a library dependency for other projects for now.
+
 mod client;
 mod config;
 mod document;
@@ -8,5 +14,7 @@ mod parser;
 mod providers;
 mod resolver;
 mod server;
+#[doc(hidden)]
 pub use crate::server::PathServer;
+#[doc(hidden)]
 pub use config::{Completion, Config, Highlight};
